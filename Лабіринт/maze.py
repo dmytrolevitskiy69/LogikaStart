e0d8ad55608658d5c6f839d1c91957138a305dbe
dmytrolevitskiy69 <dimonlevitskiy19022010@gmail.com>
2024-11-23 11:50:47 +0200

    Add files via upload

--- Лабіринт/maze.py
+++ Лабіринт/maze.py
@@ -1,45 +1,79 @@
-from pygame import*
-window = display.set_mode((700, 500))
-display.set_caption("Лабіринт")
-background = transform.scale(image.load("background.jpg"),(700, 500))
-
-
+from pygame import *
+ 
+#клас-батько для спрайтів
 class GameSprite(sprite.Sprite):
+    #конструктор класу
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
-
-        self.image = transform.scale(image.load(player_image), (65,65))
+        #кожен спрайт повинен зберігати властивість image - зображення
+        self.image = transform.scale(image.load(player_image), (65, 65))
         self.speed = player_speed
-
+        #кожен спрайт повинен зберігати властивість rect - прямокутник, в який він вписаний
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-
+ 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-
-player = GameSprite('hero.png', 5, 420, 4)
-monster = GameSprite('cyborg.png', 620, 280, 2)
-final = GameSprite('treasure.png', 480, 420, 0)
-
+class Player(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_LEFT] and self.rect.x>5:
+            self.rect.x-=self.speed
+        if keys_pressed[K_RIGHT] and self.rect.x<620:
+            self.rect.x+=self.speed
+        if keys_pressed[K_UP] and self.rect.y>5:
+            self.rect.y-=self.speed
+        if keys_pressed[K_DOWN] and self.rect.y<420:
+            self.rect.y+=self.speed
+class Enemy(GameSprite):
+    def update(self):
+        if self.rect.x<=470:
+            self.direction='right'
+        if self.rect.x>=615:
+            self.direction='left'
+        if self.direction=='left':
+            self.rect.x-=self.speed
+        else:
+            self.rect.x+=self.speed
+ 
+#Ігрова сцена:
+win_width = 700
+win_height = 500
+ 
+window = display.set_mode((win_width, win_height))
+display.set_caption("Maze")
+background = transform.scale(image.load("background2.jpg"), (win_width, win_height))
+ 
+#Персонажі гри:
+player = Player('hero.png', 5, win_height - 80, 4)
+monster = Enemy('cyborg.png', win_width - 80, 280, 2)
+final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
+ 
+game = True
+clock = time.Clock()
+FPS = 60
+ 
+#музика
 mixer.init()
 mixer.music.load('jungles.ogg')
 mixer.music.play()
-kick = mixer.Sound('kick.ogg')
-FPS = 60
-clock = time.Clock()
-
-
-game = True
+kick=mixer.Sound('kick.ogg')
+finish=False 
 while game:
-    window.blit(background, (0, 0))
-
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish!=True:
+        window.blit(background,(0, 0))
+        player.update()
+        monster.update()
+
     player.reset()
     monster.reset()
     final.reset()
 
+   
+     
     display.update()
     clock.tick(FPS)
