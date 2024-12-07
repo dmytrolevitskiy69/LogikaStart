8722d70e473969b267ca81e198ef3ad3cbc0c6a6
dmytrolevitskiy69 <dimonlevitskiy19022010@gmail.com>
2024-12-07 11:04:02 +0200

    Add files via upload

--- Лабіринт/maze.py
+++ Лабіринт/maze.py
@@ -36,29 +36,58 @@ class Enemy(GameSprite):
             self.rect.x-=self.speed
         else:
             self.rect.x+=self.speed
- 
+class Wall(sprite.Sprite):
+    def __init__(self,color_1,color_2,color_3,wall_x,wall_y,wall_width,wall_height,):
+        super().__init__()
+        self.color_1 =color_1
+        self.color_2=color_2
+        self.color_3=color_3
+        self.width=wall_width
+        self.height=wall_height
+        self.image=Surface((self.width,self.height))
+        self.image.fill((color_1, color_2, color_3))
+        self.rect = self.image.get_rect()
+        self.rect.x=wall_x
+        self.rect.y=wall_y
+    def draw_wall(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
 #Ігрова сцена:
 win_width = 700
 win_height = 500
- 
+font.init()
+font = font.Font(None,70)
+win = font.render("YOU WIN!", True, (255,215,0))
+lose = font.render("YOU LOSE!", True, (240,35,33))
 window = display.set_mode((win_width, win_height))
 display.set_caption("Maze")
-background = transform.scale(image.load("background2.jpg"), (win_width, win_height))
+background = transform.scale(image.load("background.jpg"), (win_width, win_height))
  
 #Персонажі гри:
 player = Player('hero.png', 5, win_height - 80, 4)
 monster = Enemy('cyborg.png', win_width - 80, 280, 2)
 final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
- 
+# стіни
+w1 = Wall(158, 173, 123, 100, 20, 450, 10) # жовта
+w2 = Wall(158, 173, 123, 100, 480, 350, 10)
+w3 = Wall(158, 173, 123, 100, 10, 10, 380) 
+
+
+
+
+w4 = Wall(158, 173, 123, 200, 140, 340, 10)
+w5 = Wall(158, 173, 123, 200, 140, 10, 300) 
+
 game = True
 clock = time.Clock()
 FPS = 60
- 
+
 #музика
 mixer.init()
 mixer.music.load('jungles.ogg')
 mixer.music.play()
 kick=mixer.Sound('kick.ogg')
+money = mixer.Sound('money.ogg')
 finish=False 
 while game:
     for e in event.get():
@@ -68,10 +97,22 @@ while game:
         window.blit(background,(0, 0))
         player.update()
         monster.update()
+     
+        player.reset()
+        monster.reset()
+        final.reset()
 
-    player.reset()
-    monster.reset()
-    final.reset()
+        w1.draw_wall()
+        w2.draw_wall()
+        w3.draw_wall()
+        w4.draw_wall()
+        w5.draw_wall()
+    if sprite.collide_rect(player,monster) or sprite.collide_rect(player, w1)  or sprite.collide_rect(player, w2)  or sprite.collide_rect(player, w3)  or sprite.collide_rect(player, w4) or sprite.collide_rect(player, w5):
+        finish = True
+        window.blit(lose,(200,200))
+    if sprite.collide_rect(player, final):
+        finish = True
+        window.blit(win,(200,200))         
 
    
      
